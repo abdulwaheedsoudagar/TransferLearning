96d6cdce6d6ad9cd78c38a777a296d7302a90813
abdulwaheedsoudagar <abdul.1si13cs001@gmail.com>
2021-02-21 18:35:24 +0530

    Changes in files and folder

--- train.py
+++ train.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
 import torchvision
@@ -15,8 +14,6 @@ from datasetload import Cardataset
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Using", device)
-
-
 
 learning_rate = 1e-3
 batch_size = 128
